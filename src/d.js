b8aa1e5e03c3defaf288a14e1a4c264ad83b470b
uzairnaveed04 (via App) <uzairnaveed04@users.noreply.github.com>
2025-11-09 18:03:39 +0500

    Save

--- src/d.js
+++ src/d.js
@@ -1,1 +1,14 @@
-Hello world 
+// src/components/Greeting.js
+
+import React from 'react';
+
+const Greeting = ({ name }) => {
+  return (
+    <div>
+      <h1>Hello, {name}!</h1>
+      <button>Click Me</button>
+    </div>
+  );
+};
+
+export default Greeting;
